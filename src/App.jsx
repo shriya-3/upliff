3b56bfbc86213406a17797b9b2289021b8b06caa
shriya-k5 <shriyak847@gmail.com>
2025-12-07 15:19:56 -0600

    support start

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar.jsx";
 import Home from "./components/Home.jsx";
+import Support from "./components/Support.jsx";
+import CommunityPage from "./components/CommunityPage.jsx";
 
 function App() {
   return (
@@ -9,6 +11,9 @@ function App() {
       <Navbar /> 
       <Routes>
         <Route path="/" element={<Home />} /> 
+        <Route path="/home" element={<Home />} /> 
+        <Route path="/support" element={<Support />} /> 
+        <Route path="/community/:id" element={<CommunityPage />} />
       </Routes>
     </div>
   );
