488796fa3d6e01b88ca87a84ec1246ed79e0f250
shriya-k5 <shriyak847@gmail.com>
2025-12-13 17:10:45 -0600

    front and back?

--- upliff-frontend/src/App.jsx
+++ upliff-frontend/src/App.jsx
@@ -6,6 +6,7 @@ import Support from "./components/Support.jsx";
 import CommunityPage from "./components/CommunityPage.jsx";
 import Schedule from "./components/Schedule.jsx"
 import Blog from "./components/Blog.jsx";
+import PostPage from "./components/PostPage.jsx";
 
 function App() {
   return (
@@ -16,9 +17,11 @@ function App() {
         <Route path="/home" element={<Home />} /> 
         <Route path="/support" element={<Support />} /> 
         <Route path="/community/:id" element={<CommunityPage />} />
+        <Route path="/community/:id/post/:postId" element={<PostPage />} />
         <Route path="/schedule" element={<Schedule />} /> 
         <Route path="/blog" element={<Blog />} /> 
 
+        
       </Routes>
     </div>
   );
