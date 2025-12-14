94709bca1a18b0c543bfbc4e9ae5596f3779d351
tanvin8 <tanvi08.nagilla@gmail.com>
2025-12-14 01:10:13 -0600

    backend sub to main

--- upliff-frontend/src/components/PostPage.jsx
+++ upliff-frontend/src/components/PostPage.jsx
@@ -18,7 +18,7 @@ export default function PostPage() {
       .then(setPost)
       .catch(console.error);
   }, [id, postId]);
-
+  
   // Fetch comments
   useEffect(() => {
     fetch(`${API_URL}/api/posts/${postId}/comments`)
